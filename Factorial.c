7b8f2e183d393ccc26a56dc2b238bc76bfec407d
nabiranaqeeb99 <nabiranqeeb99@gmail.com>
2026-03-05 20:08:29 +0500

    Factorial

---
+++ Factorial.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, i, factorial = 1;
+    
+    printf("Enter a number: \n");
+    scanf("%d", &n);
+    
+    if (n < 0) 
+    {
+        printf("Factorial is not defined for negative numbers.\n");
+    } 
+    else if (n == 0)
+    {
+        printf("Factorial of 0 is 1.\n");
+    }
+    else
+    {
+        for (i = 1; i <= n; i++)
+     {
+            factorial *= i;
+        }
+        printf("Factorial of %d is %d\n", n, factorial);
+    }
+    
+    return 0;
+}
